9d1e74f74f7025edff861dd6e5045432d46f6a1c
Jesenia Jess <jesenia.jesscortez1594@gmail.com>
2025-08-02 19:02:59 -0600

    Notes

--- Basic/02-excersices.js
+++ Basic/02-excersices.js
@@ -2,4 +2,18 @@
 /*Este es un comentario en varias
 lineas de codigo
 */
+let name = "jess" //string
+let height = 1.55 //decimal, number
+let age = 20 //entero
+let isStudent = true //boleano
 
+console.log( typeof name) // Imprime el valor de name
+
+//una constante no se puede reasignar
+//const
+const helloWorld3 = "Hola Mundo x4"
+console.log(helloWorld3)
+
+//error
+//const helloWorld3 = "Hola Mundo x4 nuevo"
+//console.log(helloWorld3)
