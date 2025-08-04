0b573ee65d64aee8a5c6615829c35d64730f130e
Jesenia <jesscortez1594@gmail.com>
2025-08-03 22:30:13 -0600

    Notes

--- Basic/04-operatorsExcersice.js
+++ Basic/04-operatorsExcersice.js
@@ -1,2 +1,11 @@
-//crea una variable para cada operacion aritmetica
-//Imprime 5 comparaciones verdades con diferentes operadores de comparación
+//1. crea una variable para cada operacion aritmetica
+//2. crea una variable para cada tipo de operacion de asignacion que haga uso
+// de las variables utilizadas para las operaciones aritmeticas
+//3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+//4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+//5. Utiliza el operador and
+//6. Utiliza el operador or
+//7. Combina ambos operadores logicos 
+//8. añade alguna negacion 
+//9. Utiliza el operador ternario 
+//10. Combina operadores aritmeticos, de asignacion, de comparacion y logicos
