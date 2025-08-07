b2ec1e572e396763e5ff7e7680e33960a6a4a529
Jesenia Jess <jesenia.jesscortez1594@gmail.com>
2025-08-06 18:20:04 -0600

    Notess

---
+++ Basic/06-stringExercises.js
@@ -0,0 +1,10 @@
+//concatena dos cadenas dd texto 
+// muestra la longitud de una cadena de texto
+// muestra el ultimi y primer caracter de una cadena de texto
+// convierte una cadena de texto a mayusculas y minusculas
+//crea una cadena de texto en varias lineas
+//interola el valor de una variable en un string
+//reemplaza todos los espacios en blanco de un string por guiones
+//comprueba si una cadena de texto contiene una palabra concreta
+//comprueba si dos strings son iguales
+//comprueba si dos strings tienen la misma longitud
