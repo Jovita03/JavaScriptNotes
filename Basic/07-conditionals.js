338ce3c99b3533c5449d6638f67b857fbde5349a
Jesenia <jesscortez1594@gmail.com>
2025-08-15 22:52:53 -0600

    conditionals

---
+++ Basic/07-conditionals.js
@@ -0,0 +1,22 @@
+//if else if, else
+
+    //if si
+    let age = 37
+    if (age == 37) {
+        console.log("Tu edad es 37")
+    }
+    
+    // else if si no 
+    if (age == 37) {
+        console.log("Tu edad es 37")
+    } else {
+        console.log("Tu edad no es 37")
+    }
+    // else if si no,si 
+    if (age == 37) {
+        console.log("Tu edad es 37")
+    } else if  (age < 18) {
+        console.log("Es menor de edad")
+    } else {
+        console.log("Tu edad no es 37 ni menor de edad")
+    }
